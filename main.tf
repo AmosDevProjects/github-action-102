505832606a8e16ef4132d93d953aafa5df74520f
Amos Egonmwan <amos.egonmwan@hotmail.com>
2024-08-06 12:44:49 -0600

    first

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "s3-remote-backend-2024"
+    key = "github-actions/terraform.tfstate"
+    region = "us-west-2"    
+  }
+}
+
+provider "aws" {  
+    region = "us-west-2"
+}
+
+resource "aws_vpc" "actions2" {
+    cidr_block = "10.0.0.0/24"
+
+    tags = {
+        Name = "GitHub-Actions" 
+  }
+}
