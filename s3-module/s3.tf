1488d0895bbc8d7dea20b61148b6eb2082b55c9f
Amos Egonmwan <amos.egonmwan@hotmail.com>
2024-08-07 09:21:11 -0600

    BREAKING CHANGE: s3 module

--- s3-module/s3.tf
+++ s3-module/s3.tf
@@ -17,26 +17,3 @@ resource "random_integer" "bucket_int" {
   min = 1
 }
 
-/*
-# Release 2
-resource "aws_sns_topic" "topic" {
-  name   = var.topic_name
-  policy = data.aws_iam_policy_document.topic.json
-}
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.bucket.id
-
-  topic {
-    topic_arn     = aws_sns_topic.topic.arn
-    events        = ["s3:ObjectCreated:*"]
-    filter_suffix = ".log"
-  }
-}
-
-resource "aws_sns_topic_subscription" "email" {
-  endpoint  = "amos.egonmwan@hotmail.com"
-  protocol  = "email"
-  topic_arn = aws_sns_topic.topic.arn
-}
-*/
